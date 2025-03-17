2ed51f1dfe30209bac6522f35e44610da942684a
HamzaChaudhary786 <m.hamza.sattar99@gmail.com>
2025-03-17 18:43:44 +0500

    create env file and add variable

--- src/utils/FileUpload.js
+++ src/utils/FileUpload.js
@@ -1,16 +1,19 @@
 import "../../init"
 import AWS from '../../aws.config';
 
-
 // Create an S3 instance
 const s3 = new AWS.S3();
 
 // Function to upload an image and generate a URL
 export const uploadFile = async (file) => {
 
+  const uploadPath = import.meta.env.UPLOAD_PATH;
+
+
+
   const params = {
     Bucket: 'timetech-lms-prod',
-    Key: `test/${Date.now()}-${file.name}`, // Add timestamp to avoid name conflicts
+    Key: `${uploadPath}/${Date.now()}-${file.name}`, // Add timestamp to avoid name conflicts
     Body: file,
   }
 
